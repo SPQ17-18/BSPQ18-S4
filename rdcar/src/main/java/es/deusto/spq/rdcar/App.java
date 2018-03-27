ba2e79be6244089b895068da75bf96e1ca85ef13
Alejandro Ron <alejandro.ron@opendeusto.es>
2018-03-27 11:00:19 +0200

    "Despasito" added

--- rdcar/src/main/java/es/deusto/spq/rdcar/App.java
+++ rdcar/src/main/java/es/deusto/spq/rdcar/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Despacito \r\n" + 
+        		"Quiero respirar tu cuello despacito \r\n" + 
+        		"Deja que te diga cosas al oído \r\n" + 
+        		"Para que te acuerdes si no estás conmigo");
     }
 }
