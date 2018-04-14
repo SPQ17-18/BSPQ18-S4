d2026d74d1b3508043bd8d2482ac728d923622bc
gon <gon>
2018-04-14 15:59:55 +0200

    Creación de un Empleado DTO

--- rdcar/src/main/java/es/deusto/spq/server/dto/Assembler.java
+++ rdcar/src/main/java/es/deusto/spq/server/dto/Assembler.java
@@ -1,5 +1,29 @@
 package es.deusto.spq.server.dto;
 
+import java.util.ArrayList;
+
+import es.deusto.spq.server.jdo.Empleado;
+
 public class Assembler {
 
+	
+	public Assembler() {
+		
+	}
+	
+	public ArrayList<EmpleadoDTO>assembleEmpleado( ArrayList<Empleado> ArrayEmpleados){
+		
+		ArrayList<EmpleadoDTO> ArrayDTO = new ArrayList<EmpleadoDTO>();
+		
+		for(Empleado empleado : ArrayEmpleados) {
+			
+			EmpleadoDTO empleadoDTO= new EmpleadoDTO(empleado.getUsuario(), empleado.getPassword());
+			
+			ArrayDTO.add(empleadoDTO);
+		}
+		
+		
+		return ArrayDTO;
+	}
+	
 }
